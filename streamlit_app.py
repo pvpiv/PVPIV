725ed38d57c1f42ff47725a824a053cea755faba
pvpiv <fmdnyc@gmail.com>
2024-06-26 13:18:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,120 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import math
+import kaleido
+from math import sqrt
+from plotly import figure_factory 
 
-"""
-# Welcome to Streamlit!
+st.markdown(
+    """
+    <style>
+.css-m70y {display:none}{
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+s1 = dict(selector='th', props=[('text-align', 'center')])
+s2 = dict(selector='td', props=[('text-align', 'center')])
+# you can include more styling paramteres, check the pandas docs
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+# Load CSV data
+df_stats = pd.read_csv('stats.csv',encoding='latin-1')  # CSV with name, attack, defense, hp
+df_levels = pd.read_csv('cp_mod.csv',encoding='latin-1')  # CSV with level, percent
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+col1,  col2,col3, col4 = st.columns([2,1,2,2])
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+with col1:
+    # UI for selecting name, attack2, defense2, hp2, level2
+    name2 = st.selectbox('Pokemon', df_stats['Name'])
+    attack2 =st.slider('Attack IV', 0, 15, 15)
+    defense2 = st.slider('Defense IV', 0, 15, 15)
+    hp2 = st.slider('HP IV', 0, 15, 15)
+    #level2 = st.slider('Select Level', 0, 51, 25)
+    if st.button('Generate CP Table'):
+        run_calc = True
+    else:
+        run_calc = False
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+if run_calc:
+    results = []
+    for level in range(1,52):
+    # Find records in the CSVs
+        character_stats = df_stats[df_stats['Name'] == name2].iloc[0]
+        level_percent = df_levels[df_levels['Level'] == level].iloc[0]['CPM']
+    
+        # Calculation
+        total_attack = ((character_stats['Attack'] + attack2) * level_percent)
+        total_defense = (sqrt((character_stats['Defense'] + defense2 ) * level_percent))
+        total_hp = (sqrt((character_stats['HP'] + hp2) * level_percent))
+    
+        cp = max(math.floor((total_attack * total_defense * total_hp) / 10),10)
+        results.append({'Level': level, 'CP': cp})
+        
+        
+    results_df = pd.DataFrame(results)
+    #results_df.set_index('Level', inplace=True)
+    with col3:
+        
+        table1 = results_df[0:25].style.hide(axis="index").set_table_styles([s1,s2]).to_html()     
+        st.write(f'{table1}', unsafe_allow_html=True)
+        #st.sidebar.write("CP Values by Level", results_df)
+        #st.markdown(results_df.style.hide(axis="index").to_html(), unsafe_allow_html=True)
+        #st.write(results_df)
+    with col4 :
+        
+        table2 = results_df[25:52].style.hide(axis="index").set_table_styles([s1,s2]).to_html()     
+        st.write(f'{table2}', unsafe_allow_html=True)
+    with col1:
+        @st.cache_data
+        def convert_df(df):
+           return df.to_csv(index=False).encode('utf-8')
+        
+        def createImage(df):
+            fig = figure_factory.create_table(results_df)
+            fig.update_layout(autosize=True)
+            fig.write_image(str(name2 + ".png"), scale=2, engine="kaleido")
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+        csv = convert_df(results_df)
+        
+        createImage(results_df)
+        with open(str(name2 + ".png"), "rb") as file:
+            st.download_button(
+                "Download as Image",
+                data=file,
+                file_name= str(name2 + ".png"),
+                mime="image/png",
+            )
+
+        st.download_button(
+           "Save csv",
+           csv,
+           str(name2 + ".csv"),
+           "text/csv",
+           key='download-csv'
+        )
+    run_calc = True
+                
+
+
+
+                # Streamlit application
+        #st.write("DataFrame:")
+        #st.write(df)
+    
+        # Download button
