20fe785018e8c286dc09abc89924d82ceb61e26e
pvpiv <fmdnyc@gmail.com>
2024-07-29 11:09:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,7 +81,7 @@ def poke_search():
         
 with col1:
     # UI for selecting name, attack2, defense2, hp2, level2
-    #load_new(streamlit_analytics.counts,st.secrets["fb_col"])
+    load_new(streamlit_analytics.counts,st.secrets["fb_col"])
     streamlit_analytics.start_tracking()
     today = date.today()
     name2 = st.selectbox(label = today.strftime("%m/%d/%y"),options = df_stats['Name'],label_visibility = 'hidden',on_change = poke_search,key="poke_choice")
@@ -158,7 +158,9 @@ if run_calc:
            key='download-csv'
         )
     run_calc = True
-                
+streamlit_analytics.start_tracking()
+
+streamlit_analytics.stop_tracking(unsafe_password=st.secrets['pass'])
 
 
 
